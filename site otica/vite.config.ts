779c12d04d66f29e923b99d825e68c0cc950968f
Fabio <132022392+cadefab1n@users.noreply.github.com>
2026-04-16 13:54:22 -0300

    Update vite.config.ts

--- site otica/vite.config.ts
+++ site otica/vite.config.ts
@@ -1,13 +1,13 @@
 
 
 
-import tailwindcss from '@tailwindcss/vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
-import {defineConfig, loadEnv} from 'vite';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  base: "/otica/",
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [react()],
+  }
 })
   const env = loadEnv(mode, '.', '');
   return {
